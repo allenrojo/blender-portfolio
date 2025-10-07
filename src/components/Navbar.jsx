01d872fb687dae00fe49fd204bf95186102acad9
Allen <allenrojo31@gmail.com>
2025-10-07 22:31:28 +0800

    reverted changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,19 @@
-import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-  const [activeSection, setActiveSection] = useState("home");
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = ["home", "projects", "contact"];
-      let current = activeSection;
-
-      for (const section of sections) {
-        const element = document.getElementById(section);
-        if (element) {
-          const rect = element.getBoundingClientRect();
-          if (
-            rect.top <= window.innerHeight / 2 &&
-            rect.bottom >= window.innerHeight / 2
-          ) {
-            current = section;
-            break;
-          }
-        }
-      }
-      setActiveSection(current);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Initial call to set active section on load
-    handleScroll();
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [activeSection]);
   return (
     <div className="navbar-container">
       <nav className="navbar">
-        <a
-          href="#home"
-          className={`nav-link ${activeSection === "home" ? "active" : ""}`}
-        >
+        <NavLink to="/#home" className="nav-link">
           [HOME]
-        </a>
-        <a
-          href="#projects"
-          className={`nav-link ${activeSection === "projects" ? "active" : ""}`}
-        >
+        </NavLink>
+        <NavLink to="/#projects" className="nav-link">
           [PROJECTS]
-        </a>
-        <a
-          href="#contact"
-          className={`nav-link ${activeSection === "contact" ? "active" : ""}`}
-        >
+        </NavLink>
+        <NavLink to="/#contact" className="nav-link">
           [CONTACT]
-        </a>
+        </NavLink>
       </nav>
       <img className="logo" src="/logo.svg"></img>
     </div>
